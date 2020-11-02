607ae937800a653bf8fa54afe87e3a7090fa04f1
Emmett Racine <ewr0002@uah.edu>
2020-11-02 19:32:18 +0000

    Added Functions
    addPlayer(QString name)
    removePlayer(QString name)
    startGame()
    closeLobby()
    
    Added functionality to the QDialogButton element
    (clicking OK runs startGame(), and clicking Cancel runs closseLobby())
    
    Added functionality to the player List container object
    (Updates Player list containter with list model when players are added
    or remooved via addPPlayer() and remoovePlayer())
    
    
    git-svn-id: svn+ssh://blackhawk.ece.uah.edu/home/work/cpe353-20f/team6/ffflamingos@10 001bb9e6-f774-45d5-8e46-f6368fa79ee8

--- src/manageroom.cpp
+++ src/manageroom.cpp
@@ -6,9 +6,36 @@ ManageRoom::ManageRoom(QWidget *parent) :
     ui(new Ui::ManageRoom)
 {
     ui->setupUi(this);
+
+    ui->players->setModel(&model);
+
+    connect(ui->buttonBox,&QDialogButtonBox::accepted,this,&ManageRoom::startGame);
+    connect(ui->buttonBox,&QDialogButtonBox::rejected,this,&ManageRoom::closeLobby);
 }
 
 ManageRoom::~ManageRoom()
 {
     delete ui;
+}
+
+void ManageRoom::addPlayer(QString name)
+{
+    list.append(name);
+    model.setStringList(list);
+}
+
+void ManageRoom::removePlayer(QString name)
+{
+    list.removeOne(name);
+    model.setStringList(list);
+}
+
+void ManageRoom::startGame()
+{
+
+}
+
+void ManageRoom::closeLobby()
+{
+
 }
