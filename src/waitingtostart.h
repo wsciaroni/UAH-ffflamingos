2051c978d2d357f86909350130ece26ee54687f7
Emmett Racine <ewr0002@uah.edu>
2020-11-02 19:34:10 +0000

    Added Function
    void disconnect()
    
    
    git-svn-id: svn+ssh://blackhawk.ece.uah.edu/home/work/cpe353-20f/team6/ffflamingos@11 001bb9e6-f774-45d5-8e46-f6368fa79ee8

--- src/waitingtostart.h
+++ src/waitingtostart.h
@@ -12,11 +12,29 @@ class WaitingToStart : public QDialog
     Q_OBJECT
 
 public:
+
+    /**
+     * @brief WaitingToStart
+     * @param parent
+     */
     explicit WaitingToStart(QWidget *parent = nullptr);
+    /**
+     *Destructor
+     */
     ~WaitingToStart();
 
 private:
+
+    ///ui pointer
     Ui::WaitingToStart *ui;
+
+private slots:
+
+    /**
+    @todo Needs to run the MannageRoom::removePlayer(username) function for host's instance of the window
+    as well as disconnect the player.
+    */
+    void disconnect();
 };
 
 #endif // WAITINGTOSTART_H
