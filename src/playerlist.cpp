962ad0d88458b25af5c6c04f39789f902a5652b6
Jeffrey Davis <jnd0009@uah.edu>
2020-11-05 03:24:15 +0000

    git-svn-id: svn+ssh://blackhawk.ece.uah.edu/home/work/cpe353-20f/team6/ffflamingos@26 001bb9e6-f774-45d5-8e46-f6368fa79ee8

--- src/playerlist.cpp
+++ src/playerlist.cpp
@@ -1,2 +1,58 @@
 #include "playerlist.h"
 
+PlayerList::PlayerList() {
+
+}
+
+PlayerList::~PlayerList() {
+
+}
+
+bool PlayerList::addPlayer(PlayerModel *playerModel) {
+    if(playerList.contains(playerModel)) {
+        // Return false, because we don't want to add a player twice
+        return false;
+    } else {
+        playerList.insert(playerModel, playerModel->getUID());
+        return true; // Successful insertion
+    }
+}
+
+bool PlayerList::removePlayer(PlayerModel *playerModel) {
+    if(playerList.contains(playerModel)) {
+        playerList.remove(playerModel);
+        return true; // Successful removal
+    } else {
+        return false; // Unsuccessful removal
+    }
+}
+
+bool PlayerList::removePlayer(int uid) {
+    if(playerList.key(uid) && playerList.contains(playerList.key(uid))) {
+        playerList.remove(playerList.key(uid)); // Remove the key
+        return true; // Successfully found and removed
+    } else {
+        return false; // If unsuccessful (not found) return false
+    }
+}
+
+PlayerModel* PlayerList::getPlayer(int uid) {
+    return playerList.key(uid); // Return the player model with uid
+}
+
+PlayerModel* PlayerList::getPlayerByPosition(PlayerPosition position) {
+    // Iterate through the map, and if we find a position return it.
+
+    for(PlayerModel* temp : playerList.keys()) {
+        if(temp->getPositionId() == position) {
+            return temp; // Return the player at the given position
+        }
+    }
+
+    return nullptr; // If no player at the given position, return null
+}
+
+int PlayerList::getPlayerCount() {
+    return playerList.keys().length();
+}
+
--- src/playerlist.h
+++ src/playerlist.h
@@ -1,12 +1,52 @@
 #ifndef PLAYERLIST_H
 #define PLAYERLIST_H
 
+#include <QMap>
+#include "hostmodel.h"
+#include "playermodel.h"
 
 class PlayerList
 {
+private:
+    QMap<PlayerModel*, int> playerList;
+
 public:
     PlayerList();
     ~PlayerList();
+
+    /*
+     * Add a player model to the QMap
+     * returns true if successful
+     */
+    bool addPlayer(PlayerModel* playerModel);
+
+    /*
+     * Remove a player model from the QMap
+     * returns true if successful
+     */
+    bool removePlayer(PlayerModel* playerModel);
+
+    /*
+     * Remove a player model from the QMap based on uid
+     * Returns true if successful
+     */
+    bool removePlayer(int uid);
+
+    /*
+     * Return the player model based on uid
+     */
+    PlayerModel* getPlayer(int uid);
+
+    /*
+     * Return a player model based on the given position
+     */
+    PlayerModel* getPlayerByPosition(PlayerPosition position);
+
+    /*
+     * Returns the current player count
+     */
+    int getPlayerCount();
+
 };
 
 #endif // PLAYERLIST_H
