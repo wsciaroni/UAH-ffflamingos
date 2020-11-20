2e7a49cc0bef97478b848a7b76e06cd5e86234cc
Tommy Baker <tab0043@uah.edu>
2020-11-20 02:28:00 +0000

    git-svn-id: svn+ssh://blackhawk.ece.uah.edu/home/work/cpe353-20f/team6/ffflamingos@69 001bb9e6-f774-45d5-8e46-f6368fa79ee8

--- src/determineroledialog.cpp
+++ src/determineroledialog.cpp
@@ -9,13 +9,18 @@ DetermineRoleDialog::DetermineRoleDialog(QWidget* parent)
           &DetermineRoleDialog::moveForward);
   connect(ui->buttonBox, &QDialogButtonBox::rejected, this,
           &DetermineRoleDialog::cancel);
+
+  QRegularExpression userName("[a-zA-Z0-9]{1,10}");
+  QRegularExpressionValidator* userNameValidator =
+          new QRegularExpressionValidator(userName, ui->userName);
+  ui->userName->setValidator(userNameValidator);
 }
 
 DetermineRoleDialog::~DetermineRoleDialog() { delete ui; }
 
 void DetermineRoleDialog::moveForward() {
   if (!(ui->userName->text().isEmpty())) {
-    if (ui->createGame->isChecked()) {
+    if (ui->createGame->isChecked()) {   
       emit this->DRPlayAsHost(ui->userName->text());
     } else {
       emit this->DRPlayAsGuest(ui->userName->text());
